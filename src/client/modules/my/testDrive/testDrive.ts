c2a51af39150ccecc86e6cd3ec4eb8c9e734c660
mshanemc <shane.mclaughlin@salesforce.com>
2019-09-10 17:19:58 -0500

    get form input and post

--- src/client/modules/my/testDrive/testDrive.ts
+++ src/client/modules/my/testDrive/testDrive.ts
@@ -1,8 +1,23 @@
 import { LightningElement } from 'lwc';
 
 export default class TestDrive extends LightningElement {
-    formSubmit() {
-        // get the values
-        // store in pgdb
+    async formSubmit(event) {
+        const postBody = {};
+        // console.log(event.detail);
+        // event.preventDefault();
+        if (event.detail.label === 'Submit') {
+            this.template.querySelectorAll('input').forEach(input => {
+                postBody[input.name] = input.value;
+            });
+            this.template.querySelectorAll('select').forEach(input => {
+                postBody[input.name] = input.value;
+            });
+            console.log(postBody);
+        }
+
+        await fetch('/testdrive', {
+            method: 'post',
+            body: JSON.stringify(postBody)
+        });
     }
 }
